c33117d1a963c8c69abf29227396ea0f3438bc67
DDury <87566183+DDury@users.noreply.github.com>
2025-02-20 10:17:53 +0900

    real start

---
+++ 1.NuxtBasic/1.GetStart/app/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLayout :name="another">
+    <NuxtPage></NuxtPage>
+  </NuxtLayout>
+</template>
+
+<script setup>
+const another = 'another'
+</script>
---
+++ 1.NuxtBasic/1.GetStart/app/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+<nav>
+    <ul>
+      <li>
+        <NuxtLink to="/">Main page</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="about">About page</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+  This is default layout!
+  <slot></slot>
+</template>
