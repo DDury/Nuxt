bce63cf59b55492e520b18eed70665671962b8d9
DDury <87566183+DDury@users.noreply.github.com>
2025-02-20 23:15:23 +0900

    modules

---
+++ 2.modules/app/pages/blog/[id].vue
@@ -0,0 +1,7 @@
+<template>
+  <div>This is my private blog [{{ route.params.id }}]</div>
+</template>
+
+<script setup>
+const route = useRoute();
+</script>
---
+++ 2.modules/app/pages/blog/index.vue
@@ -0,0 +1,1 @@
+<template><div>This is main blog</div></template>
