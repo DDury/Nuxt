0b4508836ca33d4493063a32abbd3c5ac5425702
DDury <87566183+DDury@users.noreply.github.com>
2025-02-24 07:36:53 +0900

    verify

--- 2.modules/app/pages/repos.vue
+++ 2.modules/app/pages/repos.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
     <p class="pb-5">My repository</p>
+
+    <UPage>
+      <template #default>
+        <h1 class="text-xl font-bold mb-3">대시보드</h1>
+        <UPageGrid class="mb-5">
+          <UCard>
+            <h3 class="text-lg font-semibold">위젯 1</h3>
+            <p>대시보드 위젯 내용</p>
+          </UCard>
+          <UCard>
+            <h3 class="text-lg font-semibold">위젯 2</h3>
+            <p>대시보드 위젯 내용</p>
+          </UCard>
+        </UPageGrid>
+      </template>
+    </UPage>
+
     <ul class="grid grid-cols-1 gap-4">
       <li
         v-for="repo in repos"
--- 2.modules/nuxt.config.js
+++ 2.modules/nuxt.config.js
@@ -6,12 +6,19 @@ export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4,
   },
-
+  extends: ["@nuxt/ui-pro"],
+  uiPro: {
+    license: "D0281163-6FB3-45CB-B1B0-30A7881F4D5C",
+  },
   modules: [
     "@nuxtjs/tailwindcss",
     "@pinia/nuxt",
     "pinia-plugin-persistedstate",
     "@vueuse/nuxt",
     "@nuxt/content",
+    "@nuxt/ui",
   ],
+  tailwindcss: {
+    exposeConfig: true, // Tailwind 설정을 UI에서 접근 가능하도록 함
+  },
 });
